e781951804db7528d927efce4eea0d6359d7163c
RyoyaOzaki <r.ozaki3018@gmail.com>
2018-12-13 13:17:05 +0900

    just updated root of routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'test' => 'tests#index' # Basic認証導入のテスト用@2018.12.13 Ozaki
+  root 'tests#index' # Basic認証導入のテスト用@2018.12.13 Ozaki
 end
