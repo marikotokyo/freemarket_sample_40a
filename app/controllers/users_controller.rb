24eae461ab64a4eb83f633c5e54ab59098ccee3b
suzuki05 <>
2018-12-18 16:34:01 +0900

    add_jquery

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,13 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
   def choice
     render layout: 'layout_content'
   end
 
   def logout
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users do
     get :choice, on: :collection
-    post :follow, on: :member
     get :logout, on: :collection
   end
 
