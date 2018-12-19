be140386b9df9e6a850606ee04ae9ccad970ca44
kei-iwai-koji <iw.kj.16@gmail.com>
2018-12-19 17:04:08 +0900

    create_model and add_action_controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,27 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
 
   def index
+  end
+
+  def new
+    render layout: 'layout_content'
+  end
+
+  def create
+    Item.create(name: item_params[:name], description: item_params[:description], price: item_params[:price], condition: item_params[:condition], shipping_from: item_params[:shipping_from], shipping_date: item_params[:shipping_date], shipping_fee: item_params[:shipping_fee], shipping_way: item_params[:shipping_way], user_id: current_user.id)
   end
 
   def show
   end
 
-  def new
-    render layout: 'layout_content'
+  private
+  def item_params
+    params.permit(:name, :description, :price, :condition, :shipping_from, :shipping_date, :shipping_fee, :shipping_way)
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181214072008) do
+ActiveRecord::Schema.define(version: 20181219073807) do
+
+  create_table "items", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "name", null: false
+    t.text "description", null: false
+    t.integer "price", null: false
+    t.integer "condition", null: false
+    t.integer "shipping_from", null: false
+    t.integer "shipping_date", null: false
+    t.integer "shipping_fee", null: false
+    t.integer "shipping_way", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "email", default: "", null: false
@@ -25,4 +40,5 @@ ActiveRecord::Schema.define(version: 20181214072008) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
