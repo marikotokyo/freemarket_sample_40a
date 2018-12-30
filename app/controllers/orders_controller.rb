4c2a99fcc843fdbeea5d78aa9b7bcb399e233232
kei-iwai-koji <iw.kj.16@gmail.com>
2018-12-30 15:46:22 +0900

    add show-view

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -12,8 +12,9 @@ class OrdersController < ApplicationController
     @order = Order.create(item_id: params[:item_id], user_id: current_user.id, status: 0)
   end
 
-  # def show
-  # end
+  def show
+    @order = Order.find_by(item_id: params[:item_id])
+  end
 
   # def edit
   # end
