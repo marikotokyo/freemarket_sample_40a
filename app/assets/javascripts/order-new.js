076c9f62b5452b32613af533c7cffe0907c4a6da
kei-iwai-koji <iw.kj.16@gmail.com>
2019-01-04 15:46:44 +0900

    create js file

---
+++ app/assets/javascripts/order-new.js
@@ -0,0 +1,14 @@
+// $(document).on('turbolinks:load',function() {
+//   $('.buy-main-content__process-msg').on('click', function() {
+//     console.log('777');
+//     return false;
+//     // var usersCard
+//     var user_id = $
+//     $.ajax({
+//       type: '',
+//       url: '',
+//       data: {id: }
+//     })
+//   })
+// });
+
