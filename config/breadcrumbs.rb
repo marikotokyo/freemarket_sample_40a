7722fbab86331bd5ecffae62d2efa7f58ea1e5b6
RyoyaOzaki <r.ozaki3018@gmail.com>
2019-01-07 20:32:01 +0900

    PANKUZU has been implemented.

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -1,28 +1,25 @@
 crumb :root do
-  link "Home", root_path
+  link "メルカリ", root_path
 end
 
-# crumb :projects do
-#   link "Projects", projects_path
-# end
+crumb :mypage do
+  link "マイページ", user_path
+end
 
-# crumb :project do |project|
-#   link project.name, project_path(project)
-#   parent :projects
-# end
+crumb :categories do
+  link "カテゴリ一覧", categories_path
+end
 
-# crumb :project_issues do |project|
-#   link "Issues", project_issues_path(project)
-#   parent :project, project
-# end
+crumb :brands do
+  link "ブランド一覧", brands_path
+end
 
-# crumb :issue do |issue|
-#   link issue.title, issue_path(issue)
-#   parent :project_issues, issue.project
-# end
+crumb :profile do
+  link "プロフィール", edit_user_path
+  parent :mypage
+end
 
-# If you want to split your breadcrumbs configuration over multiple files, you
-# can create a folder named `config/breadcrumbs` and put your configuration
-# files there. All *.rb files (e.g. `frontend.rb` or `products.rb`) in that
-# folder are loaded and reloaded automatically when you change them, just like
-# this file (`config/breadcrumbs.rb`).
+crumb :category do |category|
+  link "#{category.name}", category_path(params[:id])
+  parent :categories
+end
